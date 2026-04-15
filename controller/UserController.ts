49af05892f788191d51f0238d6be91fdf5ee7a8a
Gustavo Tavares Ferreira <pet0160450@aluno.firjansenaisesi.com.br>
2026-04-15 20:30:57 -0300

    delete att

--- controller/UserController.ts
+++ controller/UserController.ts
@@ -19,6 +19,7 @@ export class UserController {
         .json({ error: "Ocorreu um erro inesperado ao criar o usuário." });
     }
   }
+
   async list(req: Request, res: Response) {
     try {
       const users = await this.userRepository.find();
@@ -32,4 +33,26 @@ export class UserController {
         .json({ error: "Ocorreu um erro inesperado ao listar os usuários." });
     }
   }
-}
+  async delete(req: Request, res: Response) {
+    try {
+      const id  = Number(req.params.id);
+      if (isNaN(id)) {
+        return res.status(400).json({ error: "ID inválido." });
+      }
+      const result = await this.userRepository.delete(id);
+      if (result.affected === 0) {
+        return res.status(404).json({ error: "Usuário não encontrado." });
+      }
+      return res.status(204).send();
+    }
+      catch (error: unknown) {
+        if (error instanceof Error) {
+          return res.status(400).json({ error: error.message });
+        }
+        return res
+          .status(500)
+          .json({ error: "Ocorreu um erro inesperado ao deletar o usuário." });
+      }
+    }
+  }
+
