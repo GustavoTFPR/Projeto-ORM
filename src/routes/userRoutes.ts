5d0723e30a4d42a5fdb37700636515d17d37f9d4
Gustavo Tavares Ferreira <pet0160450@aluno.firjansenaisesi.com.br>
2026-04-15 21:26:21 -0300

    get by id

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -8,5 +8,6 @@ router.post("/", (req, res) => userControler.create(req, res));
 router.get("/", (req, res) => userControler.list(req, res));
 router.delete("/:id", (req, res) => userControler.delete(req, res));
 router.patch("/:id", (req, res) => userControler.update(req, res));
+router.get("/:id", (req, res) => userControler.getById(req, res));
 
 export const userRoutes = router;
