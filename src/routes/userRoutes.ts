dd395af864037308c1bf097c9592e505edee14a9
Gustavo Tavares Ferreira <pet0160450@aluno.firjansenaisesi.com.br>
2026-04-16 19:29:18 -0300

    Projeto Type ORM finalizado

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,9 +5,11 @@ const router = Router();
 const userControler = new UserController();
 
 router.post("/", (req, res) => userControler.create(req, res));
-router.get("/", (req, res) => userControler.list(req, res));
-router.delete("/:id", (req, res) => userControler.delete(req, res));
+router.patch("/:id/toggle", (req, res) => userControler.toggleActive(req, res));
 router.patch("/:id", (req, res) => userControler.update(req, res));
-router.get("/:id", (req, res) => userControler.getById(req, res));
+router.get("/", (req, res) => userControler.list(req, res));
+router.get("/active", (req, res) => userControler.listActive(req, res));
+router.get("/:id", (req, res) => userControler.listById(req, res));
+router.delete("/:id", (req, res) => userControler.delete(req, res));
 
 export const userRoutes = router;
