b1fecf911cb8ebae08a09973dcf2d272bf8d9bb7
Gustavo Tavares Ferreira <pet0160450@aluno.firjansenaisesi.com.br>
2026-05-05 19:49:17 -0300

    resolvendo auth controller

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -1,60 +1,37 @@
-import type { NextFunction, Request, Response } from "express";
-import { BadRequestError, NotFoundError } from "../helpers/apiError";
-import { PostService } from "../service/PostService";
+import { Request, Response, NextFunction } from "express";
+import { AppDataSource } from "../data-source";
+import { User } from "../entity/User";
+import { BadRequestError } from "../helpers/apiError";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-export class PostController {
-  private postService = new PostService();
-
-  list = async (req: Request, res: Response, next: NextFunction) => {
+export class AuthController {
+  private userRepository = AppDataSource.getRepository(User);
+  login = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const posts = await this.postService.listAll();
-      return res.json(posts);
-    } catch (error: unknown) {
-      next(error);
-    }
-  };
+      const { email, password } = req.body;
+      const user = await this.userRepository.findOne({
+        where: { email },
+        select: ["id", "firstName", "password", "role"],
+      });
 
-  create = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { title, content } = req.body;
-      const userId = req.user_id;
-      if (userId && isNaN(userId)) {
-        throw new BadRequestError("Id do usuário inválido");
+      if (!user || !(await bcrypt.compare(password, user.password))) {
+        throw new BadRequestError("E-mail ou senha inválidos");
       }
-      await this.postService.validateSchema(req.body);
-      const newPost = await this.postService.create(title, content, userId!);
-      return res.status(201).json(newPost);
-    } catch (error: unknown) {
-      next(error);
-    }
-  };
 
-  update = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const postId = Number(req.params.id);
-      const userId = req.user_id;
-      if (isNaN(postId)) {
-        throw new BadRequestError("Id do post inválido");
-      }
-      await this.postService.validateSchema(req.body, true);
-      const post = await this.postService.update(postId, userId!, req.body);
-      return res.status(200).json(post);
-    } catch (error: unknown) {
-      next(error);
-    }
-  };
-
-  delete = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const id = Number(req.params.id);
-      const userId = req.user_id;
-      const userRole = req.user_role;
-      if (isNaN(id)) {
-        throw new BadRequestError("ID inválido");
-      }
-      await this.postService.delete(id, userId!, userRole!);
-      return res.status(204).send();
-    } catch (error: unknown) {
+      const token = jwt.sign(
+        { id: user.id, role: user.role },
+        process.env.JWT_PASS ?? "secret",
+        { expiresIn: "8h" }
+      );
+      return res.json({
+        user: {
+          name: user.firstName,
+          role: user.role,
+        },
+        token,
+      });
+    } catch (error) {
       next(error);
     }
   };
