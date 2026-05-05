741be63dfcefc8fff4268144a47b2e2870af8844
Gustavo Tavares Ferreira <pet0160450@aluno.firjansenaisesi.com.br>
2026-05-05 19:45:14 -0300

    adcionando permissoes de usuario

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -1,31 +1,60 @@
-import { Request, Response, NextFunction } from "express";
-import { AppDataSource } from "../data-source";
-import { User } from "../entity/User";
-import { BadRequestError } from "../helpers/apiError";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
+import type { NextFunction, Request, Response } from "express";
+import { BadRequestError, NotFoundError } from "../helpers/apiError";
+import { PostService } from "../service/PostService";
 
-export class AuthController {
-  private userRepository = AppDataSource.getRepository(User);
-  login = async (req: Request, res: Response, next: NextFunction) => {
+export class PostController {
+  private postService = new PostService();
+
+  list = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const { email, password } = req.body;
-      const user = await this.userRepository.findOne({
-        where: { email },
-        select: ["id", "password"],
-      });
+      const posts = await this.postService.listAll();
+      return res.json(posts);
+    } catch (error: unknown) {
+      next(error);
+    }
+  };
 
-      if (!user || !(await bcrypt.compare(password, user.password))) {
-        throw new BadRequestError("E-mail ou senha inválidos");
+  create = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { title, content } = req.body;
+      const userId = req.user_id;
+      if (userId && isNaN(userId)) {
+        throw new BadRequestError("Id do usuário inválido");
       }
+      await this.postService.validateSchema(req.body);
+      const newPost = await this.postService.create(title, content, userId!);
+      return res.status(201).json(newPost);
+    } catch (error: unknown) {
+      next(error);
+    }
+  };
 
-      const token = jwt.sign(
-        { id: user.id },
-        process.env.JWT_PASS ?? "secret",
-        { expiresIn: "8h" }
-      );
-      return res.json({ token });
-    } catch (error) {
+  update = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const postId = Number(req.params.id);
+      const userId = req.user_id;
+      if (isNaN(postId)) {
+        throw new BadRequestError("Id do post inválido");
+      }
+      await this.postService.validateSchema(req.body, true);
+      const post = await this.postService.update(postId, userId!, req.body);
+      return res.status(200).json(post);
+    } catch (error: unknown) {
+      next(error);
+    }
+  };
+
+  delete = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const id = Number(req.params.id);
+      const userId = req.user_id;
+      const userRole = req.user_role;
+      if (isNaN(id)) {
+        throw new BadRequestError("ID inválido");
+      }
+      await this.postService.delete(id, userId!, userRole!);
+      return res.status(204).send();
+    } catch (error: unknown) {
       next(error);
     }
   };
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { UserController } from "../controller/UserController";
 import { authMiddleware } from "../middlewares/authMiddleware";
+import { roleMiddleware } from "../middlewares/roleMiddleware";
+import { UserRole } from "../entity/User";
 
 const router = Router();
 const userControler = new UserController();
@@ -8,9 +10,15 @@ const userControler = new UserController();
 router.get("/", userControler.list);
 router.post("/", userControler.create);
 router.patch("/", authMiddleware, userControler.update);
-router.patch("/:id/toggle", userControler.toggleActive);
+router.patch("/:id/toggle", authMiddleware, userControler.toggleActive);
 router.get("/active", userControler.listActive);
 router.get("/:id", userControler.listById);
-router.delete("/:id", userControler.delete);
+router.delete("/:id", authMiddleware, userControler.delete);
+router.patch(
+  "/role/:id",
+  authMiddleware,
+  roleMiddleware([UserRole.ADMIN]),
+  userControler.updateRole
+);
 
 export const userRoutes = router;
